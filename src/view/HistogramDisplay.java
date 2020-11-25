801c30b11b8e72595f17580baabd90fbf59d8350
Alexandru Robert Lazar <alex2000robert@yahoo.com>
2020-11-25 10:19:46 +0000

    Kata 5

---
+++ src/view/HistogramDisplay.java
@@ -0,0 +1,45 @@
+package view;
+
+import java.awt.Container;
+import javax.swing.JPanel;
+import model.Histogram;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+
+public class HistogramDisplay extends ApplicationFrame {
+    private final Histogram<String> histogram;
+    public HistogramDisplay(String title, Histogram<String> histogram) {
+        super(title);
+        this.histogram = histogram;
+        this.setContentPane(createPanel());
+        this.pack();
+    }
+
+    private JPanel createPanel() {
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataSet()));
+        return chartPanel;
+    }
+
+    private JFreeChart createChart(DefaultCategoryDataset dataSet) {
+        JFreeChart chart = ChartFactory.createBarChart("JFreeChart Histogram", "email domains", "nº emails", 
+                dataSet, PlotOrientation.VERTICAL, 
+                false, false, rootPaneCheckingEnabled);
+        return chart;
+    }
+    
+    private DefaultCategoryDataset createDataSet() {
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        for (String key : histogram.keySet()) {
+            dataSet.addValue(histogram.get(key), "", key);
+        }
+        return dataSet;
+    }
+    
+    public void execute() {
+        this.setVisible(true);
+    }
+}
